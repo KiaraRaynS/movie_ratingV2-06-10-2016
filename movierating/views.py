9a21b02ac95745c4e2c0b81498410c1cdcbbe157
KiaraRaynS <sherman.rayn@outlook.com>
2016-06-11 12:03:37 -0400

    Rating averages added

--- movierating/views.py
+++ movierating/views.py
@@ -30,8 +30,20 @@ def view_raters(request):
 
 def view_rater(request, rater_id):
     context = {
-            "rater": (Rater.objects.filter(id=rater_id)),
+            "rater": (Rater.objects.get(id=rater_id)),
             "movies_rated": (MovieRating.objects.filter(rater=rater_id)),
-            "moviedata": (Movie.objects.filter(id=MovieRating.movie).filter(rater=rater_id))
-            }
+            "movies_count": (MovieRating.objects.filter(rater=rater_id).count()),
+            "moviedata": (Movie.objects.filter(id=MovieRating.movie).filter(rater=Rater.id).get(id=rater_id))
+             }
     return render(request, "raterpage.html", context)
+
+
+def view_movie(request, movie_id):
+    from django.db.models import Avg
+    context = {
+            "movie": (Movie.objects.get(id=movie_id)),
+            "ratings_list": (MovieRating.objects.filter(movie=movie_id)),
+            "ratings_count": (MovieRating.objects.filter(movie=movie_id).count()),
+            "ratings_avg": (MovieRating.objects.filter(movie=movie_id).aggregate(Avg("rating")))
+            }
+    return render(request, "movie.html", context)
--- movieratings/urls.py
+++ movieratings/urls.py
@@ -18,13 +18,14 @@ from django.contrib import admin
 # view list of class pages
 from movierating.views import view_index, view_movies, view_raters
 # view instance of class page
-from movierating.views import view_rater
+from movierating.views import view_rater, view_movie
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', view_index),
     url(r'movies/$', view_movies),
     url(r'^raters/$', view_raters),
-    url(r'^raters/(?P<rater_id>\w+)/$', view_rater)
+    url(r'^raters/(?P<rater_id>\w+)/$', view_rater),
+    url(r'^movies/(?P<movie_id>\w+)/$', view_movie)
 
 ]
